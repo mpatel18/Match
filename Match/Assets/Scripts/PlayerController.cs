6c27a918a114b427fc7a51c040d50cf78ab662a5
Meekit Patel <meekitpatel@gmail.com>
2018-12-03 22:28:00 -0500

    Adding Assets and Project Settings for my game Match

---
+++ Match/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float player_speed;
+
+    [SerializeField]
+    private float camera_sensitivity; 
+
+    private PlayerMotion movement;
+
+    private void Start()
+    {
+        movement = GetComponent<PlayerMotion>(); 
+    }
+
+    private void Update()
+    {
+        // Player Movement based on World 
+        float x_axis = Input.GetAxisRaw("Horizontal");
+        float z_axis = Input.GetAxisRaw("Vertical");
+
+        Vector3 horizontal_move = transform.right * x_axis;
+        Vector3 vertical_move = transform.forward * z_axis;
+
+        // Current player movement 
+        Vector3 player_movement = (horizontal_move + vertical_move).normalized;
+        Vector3 velocity = player_movement * player_speed;
+
+        // Applyed player movement
+        movement.Move(velocity);
+
+        // Player Movement based on Camera (Turning player)
+        float camera_x_axis = Input.GetAxisRaw("Mouse X");
+
+        // Rotate object on the y_axis based on camera x_axis
+        Vector3 rotation = new Vector3(0f, camera_x_axis, 0f) * camera_sensitivity;
+
+        // Apply player rotation
+        movement.Rotate(rotation);
+
+        // Player Movement based on Camera (Camera tilt)
+        float camera_y_axis = Input.GetAxisRaw("Mouse Y");
+
+        // Rotate object on the y_axis based on camera x_axis
+        Vector3 camera_rotation = new Vector3(camera_y_axis, 0f, 0f) * camera_sensitivity;
+
+        // Apply camera rotation
+        movement.CameraRotate(-camera_rotation);
+
+
+    }
+}
---
+++ Match/Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour {
+
+    // Player Variables
+    private Vector3 current_velocity;
+    private Vector3 current_rotation;
+    private Vector3 current_camera_rotation;
+    
+    // Player Components 
+    private Rigidbody rb;
+
+    // Player Children
+    [SerializeField]
+    private Camera player_camera; 
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Moving();
+        Rotation(); 
+    }
+
+    public void Move(Vector3 velocity){ current_velocity = velocity; }
+    public void Rotate(Vector3 rotation) { current_rotation = rotation; }
+    public void CameraRotate(Vector3 camera_rotation) { current_camera_rotation = camera_rotation;  }
+
+    private void Moving()
+    {
+        if (current_velocity != Vector3.zero)
+        {
+            Vector3 new_position = transform.position + (current_velocity * Time.fixedDeltaTime);
+            rb.MovePosition(new_position);
+        }
+    }
+
+    private void Rotation()
+    {
+        Quaternion new_rotation = transform.rotation * Quaternion.Euler(current_rotation); 
+        rb.MoveRotation(new_rotation);
+
+        player_camera.transform.Rotate(current_camera_rotation); 
+    }
+}
